e01f28562169c0f42aa57108eefc2bfea509843d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-13 22:42:28 +0700

    Menyelesaikan percobaan 1 Modul4 C++

--- C++/Percobaan1_Mod4_C++.cpp
+++ C++/Percobaan1_Mod4_C++.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string>
 
-using namespace stf;
+using namespace std;
 
-int main(){
+void non_return_func(string praktikan1, string praktikan2, int kelompok) {
+    cout << "Selamat datang di Praktikum DKP 2021\n";
+    cout << praktikan1 << " dan " << praktikan2 << " adalah kelompok " << kelompok;
+}
+int return_func(int perkalian) {
+    if (perkalian > 0 || perkalian < 3) {
+        return perkalian * 3;
+    } else {
+        return perkalian * 0;
+    }
+}
 
-    return 0;
+int main() {
+    non_return_func("Nanda, "Iqbal", 1); 
+    cout << "\nhasil perkalian 1 dengan 3 adalah " << return_func(1);
 }
