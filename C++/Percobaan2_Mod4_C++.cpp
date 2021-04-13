03493f218ef1e0acf88979f782496a8a10c4f5ce
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-13 22:53:17 +0700

    Merapikan dan menyelesaikan percobaan2 Mod4 C++

--- C++/Percobaan2_Mod4_C++.cpp
+++ C++/Percobaan2_Mod4_C++.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// membuat kelas bernama MyClas
 class MyClass {
+
+// membuat non return method
 public: void non_return_method(string kelompok) {
-    cout << "halo, kami dari kelompok " << kelompok;
+    cout << "Halo, kami dari kelompok " << kelompok;
     }
+
+// membuat return method
 int with_return_method(int angka) {
     return angka*angka;
     }   
 };
+
+// ini adalah main-function
 int main() {
-MyClass myObj;
-myObj.non_return_method("28");
-cout << "\nhasil kali 28 dengan 28 adalah " << myObj.with_return_method(28);
-return 0;
+    // membuat object bernama "myObj" dari kelas "MyClass"
+    MyClass myObj;
+
+    // panggil method lewat object
+    myObj.non_return_method("28");
+    cout << "\nHasil kali 28 dengan 28 adalah " << myObj.with_return_method(28);
+    return 0;
 }
