0fe01b5dcd17191886c2501734be4a6bb9e5f354
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-15 15:02:24 +0700

    Menambahkan directory tugas, dan membuat tugas dalam bahasa Java. Membuat header dalam tugas mod 4

---
+++ Tugas Modul 4/src/com/nandaiqbalh/Main.java
@@ -0,0 +1,18 @@
+package com.nandaiqbalh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t=============================");
+        System.out.println("\t\t\t\t======= TUGAS MODUL 4 =======");
+        System.out.println("\t\t\t\t=============================");
+
+        System.out.println();
+        System.out.println("Kelompok        : XXVIII");
+        System.out.println("Shift           : 2 ");
+        System.out.println("Nama anggota 1  : MUHAMMAD ROFI'UL ANAM  - 21120120140135");
+        System.out.println("Nama anggota 2  : VEROLAND ADJIE VIRAWAN - 21120120130131");
+        System.out.println("Nama anggota 3  : NANDA IQBAL HANAFI     - 21120120130109");
+        System.out.println("Nama anggota 4  : RAMA ARYASUTA WEIHAN   - 21120120120007");
+    }
+}
