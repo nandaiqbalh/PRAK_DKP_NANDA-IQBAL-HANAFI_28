053f3ee0bb8b2b4e4fc4651e4dfd992e483649f6
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-16 17:29:08 +0700

    Membuat menu utama database

--- Tugas Modul 4/src/com/nandaiqbalh/Main.java
+++ Tugas Modul 4/src/com/nandaiqbalh/Main.java
@@ -1,5 +1,7 @@
 package com.nandaiqbalh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,21 @@ public class Main {
         System.out.println("\t\t\t======= DATABASE KELOMPOK 4 =======");
         System.out.println("\t\t\t===================================");
 
-        
+        // Membuat Scanner untuk mengambil input dari user
+        Scanner userInput = new Scanner(System.in);
+
+        String pilihanUser;
+        boolean isLanjutkan = true;
+
+        while (isLanjutkan){
+            System.out.println("\nDATABASE KELOMPOK 28\n");
+            System.out.println("1.\tNama dan NIM");
+            System.out.println("2.\tAsal daerah");
+            System.out.println("3.\tHobi");
+            System.out.println("4.\tMedia sosial");
+
+            System.out.println("/Pilihan anda : ");
+            pilihanUser = userInput.next();
+        }
     }
 }
