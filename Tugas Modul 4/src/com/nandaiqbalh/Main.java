e845b4d374a9779097e5f4e75f5f81d24279911a
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-04-16 17:39:51 +0700

    Membenahi header, menamba blok perulangan menu utama, dan menambah blok pengkondisian yang akan dipilih oleh user

--- Tugas Modul 4/src/com/nandaiqbalh/Main.java
+++ Tugas Modul 4/src/com/nandaiqbalh/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\t\t\t===================================");
-        System.out.println("\t\t\t======= DATABASE KELOMPOK 4 =======");
-        System.out.println("\t\t\t===================================");
+        System.out.println("\t\t\t====================================");
+        System.out.println("\t\t\t======= DATABASE KELOMPOK 28 =======");
+        System.out.println("\t\t\t====================================");
 
         // Membuat Scanner untuk mengambil input dari user
         Scanner userInput = new Scanner(System.in);
@@ -22,8 +22,27 @@ public class Main {
             System.out.println("3.\tHobi");
             System.out.println("4.\tMedia sosial");
 
-            System.out.println("/Pilihan anda : ");
+            System.out.print("\nPilihan anda [1-4] : ");
             pilihanUser = userInput.next();
+
+            switch (pilihanUser) {
+                case "1" :
+                    System.out.println("\n\t================");
+                    System.out.println("\t==NAMA DAN NIM==");
+                    System.out.println("\t================");
+                    break;
+                case "2" :
+                    // menu kedua
+                    break;
+                case "3" :
+                    // menu ketiga
+                    break;
+                case "4" :
+                    // menu keempat
+                    break;
+                default:
+                    System.err.println("Pilihan anda bukan 1-4!");
+            }
         }
     }
 }
